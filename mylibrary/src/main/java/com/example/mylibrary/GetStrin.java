7dbae25b67ebfdc1351df5fd42f6973b9ba1a009
dookaystar <17602142951@163.com>
2017-03-16 16:21:56 +0800

    更新版本

--- mylibrary/src/main/java/com/example/mylibrary/GetStrin.java
+++ mylibrary/src/main/java/com/example/mylibrary/GetStrin.java
@@ -8,5 +8,8 @@ public class GetStrin {
     public static String getsh(String ss) {
         return "" + ss + "东方不败";
     }
+    public static String getsh2(String ss) {
+        return "" + ss + "东方不败";
+    }
 
 }
