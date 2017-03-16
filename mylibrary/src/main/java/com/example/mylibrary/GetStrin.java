273d3fe6115a4248934e4715b31aeb69b2c61cdc
dookaystar <17602142951@163.com>
2017-03-16 15:41:00 +0800

    更新版本

---
+++ mylibrary/src/main/java/com/example/mylibrary/GetStrin.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+/**
+ * Created by Administrator on 2017/3/16.
+ */
+
+public class GetStrin {
+    public static String getsh(String ss) {
+        return "" + ss + "东方不败";
+    }
+
+}
